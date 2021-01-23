5376ae17cc37f8b912071d8c7fcb0cc5af3cf9b4
ShotaOtake <70787019+ShotaOtake@users.noreply.github.com>
2021-01-23 17:08:47 +0900

    コントローラー編集(application_controller)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
 class ApplicationController < ActionController::Base
+  # もしdeviseに関するコントローラーの処理であれば、そのときだけconfigure_permitted_parametersメソッドを実行するように設定
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  # deviseのUserモデルにパラメーターを許可 :deviseの処理名 keys: [:許可するキー]
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(
+      :sign_up, keys: [:nickname, :email, :birth_date, :first_name, :last_name, :first_name_kana, :last_name_kana]
+    )
+  end
 end
