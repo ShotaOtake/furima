2547be162be5694a5267dd68cd73f2ba447487f1
ShotaOtake <70787019+ShotaOtake@users.noreply.github.com>
2021-01-24 18:14:48 +0900

    コントローラー編集(new/ create/ ストロングパラメーター)

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -4,6 +4,38 @@ class ItemsController < ApplicationController
   end
 
   def new
+    @item = Item.new
   end
-  
+
+  # createアクションにデータ保存のための記述
+    # 保存されたときはルートパスに戻る(redirect_to)
+    # 保存されなかったときは新規投稿ページへ戻る(render)
+  def create
+    @item = Item.new(item_params)
+    if @item.valid?
+      @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  # privateメソッドにストロングパラメーターをセットし、特定の値のみを受け付けるように記述。
+  # user_idもmerge
+  def item_params
+    params.require(:item).permit(
+      :image,
+      :name,
+      :info,
+      :category_id,
+      :sales_status_id,
+      :shipping_fee_status_id,
+      :prefecture_id,
+      :scheduled_delivery_id,
+      :price
+    ).merge(user_id: current_user.id)
+  end
+
 end
--- app/models/category.rb
+++ app/models/category.rb
@@ -1,6 +1,6 @@
 # モデル内（クラス内）でActiveHashを用いる際に必要となるクラス
 # ActiveHash::Baseを継承することで、ActiveRecordと同じようなメソッドを使用できるようになる
-class Category < ActiveHash: :Base
+class Category < ActiveHash::Base
   self.data = [
     { id: 0, name: '---' },
     { id: 1, name: 'レディース' },
@@ -16,6 +16,6 @@ class Category < ActiveHash: :Base
   ]
 
   # ActiveHashを用いてhas_manyを設定するには、include ActiveHash::Associationsと記述してmoduleを取り込む
-  include ActiveHash: :Associations
+  include ActiveHash::Associations
     has_many :items
 end
