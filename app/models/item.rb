6dfb9e4b747bb45035f4fce3b417a064ef097f29
ShotaOtake <70787019+ShotaOtake@users.noreply.github.com>
2021-01-24 11:40:49 +0900

    アソシエーション(ActiveHash)

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,8 +1,24 @@
 class Item < ApplicationRecord
 
+  # <<アソシエーション>>
+
   # モデル間のアソシエーション
   belongs_to :user
   # has_one_attachedを使用してimageカラムとのアソシエーションを記述
   has_one_attached :image
+
+  # Active_hashとのアソシエーション
+  # ActiveHashを用いてアソシエーションを設定する場合は、ActiveHashで定義されているmoduleをモデルに取り込む必要がある
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :sales_status
+  belongs_to :shipping_fee_status
+  belongs_to :prefecture
+  belongs_to :scheduled_delivery
+
+  
+
+　# <<バリデーション>>
+
 
 end
