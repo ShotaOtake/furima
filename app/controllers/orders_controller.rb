6b774aa1c172c88c004c0a2addfc8804b4d6ccf6
ShotaOtake <70787019+ShotaOtake@users.noreply.github.com>
2021-01-27 22:28:50 +0900

    indexアクション編集

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,6 +1,9 @@
 class OrdersController < ApplicationController
 
+  # pay_form.rbからのインスタンス変数@orderを生成
   def index 
+    @item = Item.find(params[:item_id])
+    @order = PayForm.new
   end
-  
+
 end
