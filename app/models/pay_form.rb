ad296a191215a05916c9e147a41c0699c56f0db5
ShotaOtake <70787019+ShotaOtake@users.noreply.github.com>
2021-01-29 09:35:56 +0900

    フォームオブジェクト(バリデーション/ saveメソッド)

--- app/models/pay_form.rb
+++ app/models/pay_form.rb
@@ -4,7 +4,34 @@ class PayForm
 
   # 保存したい複数のテーブルのカラム名全てを属性値として扱えるようにする
   # フォームで利用する全ての属性(つまり、保存したい各テーブルのカラム名全て)についてゲッターとセッターを定義することで、このFormオブジェクトのインスタンスを生成した際にform_withの引数として利用できるようになる
-  attr_accessor :postal_code, :prefecture_id, :city, :address, :building, :phone_number, :user_id ,:item_id
+  attr_accessor :postal_code, :prefecture_id, :city, :address, :building, :phone_number, :user_id, :item_id
 
+  # <<バリデーション>>
+  with_options presence: true do
+    validates :postal_code, format: { with: /\A\d{3}[-]\d{4}\z/}
+    validates :prefecture_id, numericality: { other_than: 0 }
+    validates :city
+    validates :address
+    validates :phone_number, format: { with: /\A[0-9]+\z/ }, length: { maximum: 11 }
+    validates :user_id
+    validates :item_id 
+  end
 
+  def save
+    order = Order.create(
+      #保存した情報を変数orderにいれる
+      item_id: item_id,
+      user_id: user_id
+    )
+    Address.create(
+      order_id: order.id,
+      #保存された情報を持つ変数orderから、idだけを取り出し、order_idとして保存を行う
+      postal_code: postal_code,
+      prefecture_id: prefecture_id,
+      city: city,
+      address: address,
+      building: building,
+      phone_number: phone_number
+    )
+  end
 end
